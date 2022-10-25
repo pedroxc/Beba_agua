a1ca7b2210f5dbf8d428acf4d9616053d8738044
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-10-25 09:47:41 -0300

    change styles

--- background.js
+++ background.js
@@ -13,7 +13,6 @@ var opt = {
   requireInteraction: false,
 };
 
-//called when the first browser instance is started
 function onStartGo() {
   chrome.storage.sync.get(
     ["time", "soundName", "noteType", "keepNote"],
@@ -39,7 +38,6 @@ function onStartGo() {
   );
 }
 
-//initial method thats called to start the reminder process
 function go(mins) {
   var found = false;
   sleepTime = mins * 60000;
@@ -47,7 +45,6 @@ function go(mins) {
   remind();
 }
 
-//continuously called until it is stopped or another time is started
 function remind() {
   if (timer) clearTimeout(timer);
   var audio = new Audio(fileName);
@@ -67,7 +64,6 @@ function remind() {
   timer = setTimeout(remind, sleepTime);
 }
 
-//stops the current timer and rich notification if they exist
 function stop() {
   if (timer) {
     clearTimeout(timer);
@@ -78,12 +74,10 @@ function stop() {
   }
 }
 
-//save the time
 function saveTime(time) {
   chrome.storage.sync.set({ time: time });
 }
 
-//save options
 function setOpts(name, type, keepNote) {
   fileName = name;
   noteType = type;
@@ -93,5 +87,4 @@ function setOpts(name, type, keepNote) {
   chrome.storage.sync.set({ keepNote: displayIndef });
 }
 
-//start the whole shebang
 onStartGo();
